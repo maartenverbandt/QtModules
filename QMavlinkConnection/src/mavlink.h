45500f3f9c89bb8be3e5f43e49e1badb88c9568e
Maarten Verbandt <maarten.verbandt@hotmail.com>
2016-08-26 07:19:20 +0200

    Change mavlink path

--- QMavlinkConnection/src/mavlink.h
+++ QMavlinkConnection/src/mavlink.h
@@ -1,6 +1,7 @@
 #ifndef MAVLINK_CONNECTION_INCLUDE_H
 #define MAVLINK_CONNECTION_INCLUDE_H
 
-#include "/home/maarten/projects/mavlink/build/include/v1.0/microos_messages/mavlink.h"
+//#include "/home/maarten/projects/mavlink/build/include/v1.0/microos_messages/mavlink.h"
+#include "/home/maarten/projects/mavlink/build/include/v1.0/ballbot_messagesavr/mavlink.h"
 
 #endif //MAVLINK_CONNECTION_INCLUDE_H
