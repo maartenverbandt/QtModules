d0b12623b77a944a750678788cc3b4fff8ba9e84
Maarten Verbandt <maarten.verbandt@hotmail.com>
2016-09-08 20:14:38 +0200

    polarplot examples

--- QIMUWidget/examples/PolarPlot/PolarPlot.pro
+++ QIMUWidget/examples/PolarPlot/PolarPlot.pro
@@ -1,12 +1,14 @@
-QT += core
-QT -= gui
+QT += core gui widgets
 
 CONFIG += c++11
 
 TARGET = PolarPlot
-CONFIG += console
-CONFIG -= app_bundle
+CONFIG += console app_bundle
 
 TEMPLATE = app
 
 SOURCES += main.cpp
+
+DEFINES += QT_NO_PRINTER
+
+include(../../src/qimuwidget.pri)
--- QIMUWidget/examples/PolarPlot/main.cpp
+++ QIMUWidget/examples/PolarPlot/main.cpp
@@ -1,8 +1,24 @@
-#include <QCoreApplication>
+#include <QApplication>
+#include <qpolarplot.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
+
+    QPolarPlot* p = new QPolarPlot();
+    p->show();
+
+    p->setRange(10.0);
+    p->addCircle(8.0);
+    p->addCircle(4.0);
+
+    p->setSize(4);
+    p->addPoint(QPointF(-6.0,-6.0));
+    p->addPoint(QPointF(6.0,6.0));
+    p->addPoint(QPointF(-6.0,6.0));
+    p->addPoint(QPointF(6.0,-6.0));
+
+    //p->update();
 
     return a.exec();
 }
