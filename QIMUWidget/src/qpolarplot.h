182e5c95836bd6c349ade8965cd387bdcdb1bc74
Maarten Verbandt <maarten.verbandt@hotmail.com>
2016-09-08 20:12:26 +0200

    Qt update gitignore

---
+++ QIMUWidget/src/qpolarplot.h
@@ -0,0 +1,17 @@
+#ifndef QPOLARPLOT_H
+#define QPOLARPLOT_H
+
+#include <QWidget>
+
+class QPolarPlot : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QPolarPlot(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // QPOLARPLOT_H
---
+++ QIMUWidget/src/qrunningpolarplot.h
@@ -0,0 +1,11 @@
+#ifndef QRUNNINGPOLARPLOT_H
+#define QRUNNINGPOLARPLOT_H
+
+
+class qrunningpolarplot : public QPolarPlot
+{
+public:
+    qrunningpolarplot();
+};
+
+#endif // QRUNNINGPOLARPLOT_H
