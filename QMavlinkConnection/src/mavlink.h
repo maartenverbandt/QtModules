00ea0f7e51be7add33936eb0810f61aafbcabbbe
Maarten Verbandt <maarten.verbandt@hotmail.com>
2016-10-03 22:03:14 +0200

    go for robot message set

--- QMavlinkConnection/src/mavlink.h
+++ QMavlinkConnection/src/mavlink.h
@@ -3,6 +3,6 @@
 
 //#include "/home/maarten/projects/mavlink/build/include/v1.0/microos_messages/mavlink.h"
 //#include "/home/maarten/projects/mavlink/build/include/v1.0/ballbot_messagesavr/mavlink.h"
-#include "mavlink/ballbot_messagesavr/mavlink.h"
+#include "mavlink/robot_messages/mavlink.h"
 
 #endif //MAVLINK_CONNECTION_INCLUDE_H
