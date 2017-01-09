036040b9cdab564b44a5ef123c182cb71d85a21a
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-01-09 10:37:13 +0100

    Gui to test also udp sending

--- QExternalPort/examples/csvlistener/mainwindow.h
+++ QExternalPort/examples/csvlistener/mainwindow.h
@@ -5,6 +5,7 @@
 #include <QDebug>
 #include <qpositioncmdportdialog.h>
 #include <mavlink/robot_messages/mavlink.h>
+#include <QTime>
 
 namespace Ui {
 class MainWindow;
@@ -18,13 +19,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void timerEvent(QTimerEvent *);
+
 private:
     Ui::MainWindow *ui;
     QPositionCmdPortDialog* d;
+    QUdpSocket* s;
+
+signals:
+    void positionCmdSend(mavlink_position_cmd_t position_cmd);
 
 public slots:
     void positionCmdCatch(mavlink_position_cmd_t position_cmd);
 
+private slots:
+    void on_pushButton_clicked();
 };
 
 #endif // MAINWINDOW_H
