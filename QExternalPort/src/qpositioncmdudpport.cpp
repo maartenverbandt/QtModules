bdc44c54f8e620aae2eb26abdbd1ec6a4db429cf
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-01-09 10:35:56 +0100

    fill out cmd udp port class

--- QExternalPort/src/qpositioncmdudpport.cpp
+++ QExternalPort/src/qpositioncmdudpport.cpp
@@ -1,6 +1,41 @@
 #include "qpositioncmdudpport.h"
 
-QPositionCmdUdpPort::QPositionCmdUdpPort()
+QPositionCmdUdpPort::QPositionCmdUdpPort(QObject *parent):
+    QObject(parent),
+    _udp_port(new QUdpPortWidget(MAVLINK_MSG_ID_POSITION_CMD_LEN,0))
 {
 
 }
+
+mavlink_position_cmd_t QPositionCmdUdpPort::getPositionCmdPacket()
+{
+    mavlink_position_cmd_t position_cmd;
+    QByteArray packet = _udp_port->readLine();
+
+    if(packet.length()>0){
+        memcpy((char*)(&position_cmd),packet.data(),sizeof(position_cmd));
+    } else {
+        position_cmd.x = 0;
+        position_cmd.y = 0;
+        position_cmd.z = 0;
+        position_cmd.xFF = 0;
+        position_cmd.yFF = 0;
+        position_cmd.zFF = 0;
+    }
+
+    return position_cmd;
+}
+
+QVariant QPositionCmdUdpPort::getPacket()
+{
+    QVariant position_cmd;
+    position_cmd.setValue(getPositionCmdPacket());
+    return position_cmd;
+}
+
+QWidget *QPositionCmdUdpPort::getWidget()
+{
+    return _udp_port;
+}
+
+
--- QExternalPort/src/qpositioncmdudpport.h
+++ QExternalPort/src/qpositioncmdudpport.h
@@ -1,12 +1,24 @@
 #ifndef QPOSITIONCMDUDPPORT_H
 #define QPOSITIONCMDUDPPORT_H
 
-#include <QObject>
+#include <qudpportwidget.h>
+#include <qpositioncmdportinterface.h>
+#include <qdebug.h>
 
-class QPositionCmdUdpPort : public QPositionCmdPortInterface
+class QPositionCmdUdpPort : public QObject, public QPositionCmdPortInterface
 {
+    Q_OBJECT
+
 public:
-    QPositionCmdUdpPort();
+    QPositionCmdUdpPort(QObject *parent = 0);
+
+    mavlink_position_cmd_t getPositionCmdPacket();
+    QVariant getPacket();
+    QWidget* getWidget();
+
+private:
+    QUdpPortWidget* _udp_port;
+
 };
 
 #endif // QPOSITIONCMDUDPPORT_H
