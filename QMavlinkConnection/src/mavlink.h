8940b318e5a271995aaedc07acc18c2724652186
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-03-10 08:29:46 +0100

    declare metatypes in mavlink.h

--- QMavlinkConnection/src/mavlink.h
+++ QMavlinkConnection/src/mavlink.h
@@ -5,4 +5,7 @@
 //#include "/home/maarten/projects/mavlink/build/include/v1.0/ballbot_messagesavr/mavlink.h"
 #include "mavlink/robot_messages/mavlink.h"
 
+Q_DECLARE_METATYPE(mavlink_gpio_t)
+Q_DECLARE_METATYPE(mavlink_position_cmd_t)
+
 #endif //MAVLINK_CONNECTION_INCLUDE_H
