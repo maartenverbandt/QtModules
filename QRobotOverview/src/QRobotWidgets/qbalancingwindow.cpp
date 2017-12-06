e17a221b898569ecafea5b86acaaae8d8f82fa77
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-12-06 12:53:43 +0100

    add recorders to balancing window

--- QRobotOverview/src/QRobotWidgets/qbalancingwindow.cpp
+++ QRobotOverview/src/QRobotWidgets/qbalancingwindow.cpp
@@ -1,4 +1,7 @@
 #include "qbalancingwindow.h"
+#include <qattituderecorder.h>
+#include <qvelocityrecorder.h>
+#include <qpositionrecorder.h>
 
 QBalancingWindow::QBalancingWindow(QAbstractRobot *robot, QWidget *parent) :
     QRobotWindow(robot, parent),
@@ -7,6 +10,9 @@ QBalancingWindow::QBalancingWindow(QAbstractRobot *robot, QWidget *parent) :
     addView(_balance->w());
 
     _tools_menu->addAction(new QShowExcitationWindowAction(this, this));
+    _recorder->add(new QAttitudeRecorder(this));
+    _recorder->add(new QVelocityRecorder(this));
+    _recorder->add(new QPositionRecorder(this));
 }
 
 QBalancingDataNodeWidget *QBalancingWindow::balance()
