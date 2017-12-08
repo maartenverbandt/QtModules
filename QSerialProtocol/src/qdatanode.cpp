6a9391e0ba6c9e52fd866c3a7c4d67252ae52aea
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-12-08 10:06:27 +0100

    bug fix: print message was not connected

--- QSerialProtocol/src/qdatanode.cpp
+++ QSerialProtocol/src/qdatanode.cpp
@@ -14,6 +14,7 @@ void QDataNode::transmitTo(QDataNode *other)
 
     QDATANODE_CONNECT(heartbeat_t);
     QDATANODE_CONNECT(gpio_t);
+    QDATANODE_CONNECT(print_t);
     QDATANODE_CONNECT(thread_info_t);
     QDATANODE_CONNECT(event_t);
     QDATANODE_CONNECT(partition_t);
@@ -24,13 +25,13 @@ void QDataNode::transmitTo(QDataNode *other)
     QDATANODE_CONNECT(attitude_cmd_t);
     QDATANODE_CONNECT(velocity_cmd_t);
     QDATANODE_CONNECT(position_cmd_t);
+    QDATANODE_CONNECT(pose_t);
     QDATANODE_CONNECT(attitude_t);
     QDATANODE_CONNECT(velocity_t);
     QDATANODE_CONNECT(position_t);
     QDATANODE_CONNECT(signal_sweptsine_t);
     QDATANODE_CONNECT(signal_multisine_t);
     QDATANODE_CONNECT(signal_steppedsine_t);
-    QDATANODE_CONNECT(pose_t);
 }
 
 void QDataNode::receiveFrom(QDataNode *other)
